3062929a5cf385ad7bbc8e79c6217284255ccf91
Matt Luke <59681386+MlukeDev@users.noreply.github.com>
2022-05-04 01:31:56 -0400

    Initial Commit

---
+++ InvidiOS/InvidiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvidiOSApp.swift
+//  InvidiOS
+//
+//  Created by Matt Luke on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct InvidiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
